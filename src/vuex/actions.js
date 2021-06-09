69475f90686f8770b85cd7c1a677a37ce3eed719
cuteKitten <450567157@qq.com>
2021-06-09 17:41:48 +0800

    a

--- src/vuex/actions.js
+++ src/vuex/actions.js
@@ -1,3 +1,5 @@
 import * as types from './mutation-types'
 
 export const setUserInfo = ({ commit }, parm) => commit(types.SET_USER_INFO, parm)
+export const setUserName = ({ commit }, parm) => commit(types.SET_USER_NAME, parm)
+
--- src/vuex/getters.js
+++ src/vuex/getters.js
@@ -1,5 +1,6 @@
 
 export const userInfo = state => state.userInfo
+export const userName = state => state.userName
 export const regionList = state => state.regionList
 export const levelList = state => state.levelList
 export const exchangeRate = state => state.exchangeRate
