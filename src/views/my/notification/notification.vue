eabd02d010f40683cde681a72b0b93a5624130a2
zjunfeng <zhangjunfeng@wsecar.com>
2021-06-20 18:07:51 +0800

    ✨ feat: 钱包、通知页面

--- src/views/my/notification/notification.vue
+++ src/views/my/notification/notification.vue
@@ -7,9 +7,26 @@
         </div>
         <div class="main">
           <div class="content">
-            <Default >
-              <div>暂无记录</div>
-            </Default>
+            <div class="panel" v-if="data.length">
+              <div class="panel-title flex">
+                <p>消息通知</p>
+                <p>{{data.length}}条记录</p>
+              </div>
+              <ul class="notify">
+                <li>
+                  <div class="flex">
+                    <div class="item-title">运单发货通知</div>
+                    <div class="item-date tr">
+                      <time>15 Jun 2021 16:32</time>
+                    </div>
+                  </div>
+                  <div class="item-desc">
+                    运单 202106131407248352858925 已完成付款
+                  </div>
+                </li>
+              </ul>
+            </div>
+            <Default v-else>暂无记录</Default>
           </div>
         </div>
    </div>
@@ -22,18 +39,46 @@
         data() {
         //这里存放数据
             return {
-                
+                data: []
             };
         },
     }
 </script>
 <style lang='less' scoped>
-    .main{
-        
+  .flex{
+    justify-content: space-between;
+  }
+  .panel-title{
+    padding: 4px 12px;
+    border-bottom: 1px solid #eeeeee;
+    >p{
+      font-size: 15px;
+      line-height: 24px;
     }
-   @media screen and (max-width: 768px) {
-    .main{
-        
+  }
+  .notify{
+    li{
+      padding: 8px 12px;
+      border-bottom: 1px dashed #eeeeee;
+      .item-title{
+        font-size: 16px;
+        line-height: 24px;
+        margin-bottom: 4px;
+      }
+      .item-date{
+        padding-left: 12px;
+        font-size: 14px;
+        line-height: 24px;
+        color: #999;
+      }
+      .item-desc{
+        font-size: 14px;
+        line-height: 18px;
+        color: #333;
+      }
+      &:last-child{
+        border: none;
+      }
     }
   }
 </style>
